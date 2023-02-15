becf90dddbb6a539abdac94b898bf608f57f571b
Florin <109854105+SFlrn22@users.noreply.github.com>
2023-02-15 15:36:49 +0200

    Add possibility to choose between black trail and rainbow trail

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const sketch = document.querySelector(".sketch")
-const firstBtn = document.querySelector("#opt-1")
-const secondBtn = document.querySelector("#opt-2")
-const thirdBtn = document.querySelector("#opt-3")
+const sketch = document.querySelector(".sketch");
+const firstBtn = document.querySelector("#opt-1");
+const secondBtn = document.querySelector("#opt-2");
+const thirdBtn = document.querySelector("#opt-3");
+const blackBtn = document.querySelector("#black");
+const rainbowBtn = document.querySelector("#rainbow");
+let colorTrail = 0;
+
+blackBtn.onclick = () => {
+    rainbow.setAttribute('style', 'opacity: 1;')
+    black.setAttribute('style', 'opacity: 0.5;')
+    colorTrail = 0;
+}
+
+rainbowBtn.onclick = () => {
+    rainbow.setAttribute('style', 'opacity: 0.5;')
+    black.setAttribute('style', 'opacity: 1;')
+    colorTrail = 1;
+}
+
 let square = document.createElement("div");
 function createSquares(number) {
     for(let i = 1; i <= number; i++){
@@ -22,17 +38,34 @@ function reset() {
 function color() {
     let squares = document.querySelectorAll(".square")
     for(let i = 0; i < squares.length; i++){
-        squares[i].onmouseenter = () => {
-            squares[i].setAttribute('style', 'background-color: black;')
+        if (colorTrail === 0){
+            squares[i].onmouseenter = () => {
+                squares[i].setAttribute('style', 'background-color: black;');
+            }
+            squares[i].onmouseleave = () => {
+                squares[i].setAttribute('style', 'background-color: black;');
+            }
         }
-        squares[i].onmouseleave = () => {
-            squares[i].setAttribute('style', 'background-color: black;')
+        else {
+            let red = Math.floor(Math.random() * 255);
+            let blue = Math.floor(Math.random() * 255);
+            let green = Math.floor(Math.random() * 255);
+            let alpha = Math.random();
+            squares[i].onmouseenter = () => {
+                squares[i].setAttribute('style', `background-color: rgba(${red}, ${green}, ${blue}, ${alpha});`);
+            }
+            squares[i].onmouseleave = () => {
+                squares[i].setAttribute('style', `background-color: rgba(${red}, ${green}, ${blue}, ${alpha});`);
+            }
         }
     }
 }
 
 firstBtn.onclick = () => {
     sketch.setAttribute('style', 'grid-template-columns: repeat(10, 1fr);');
+    firstBtn.setAttribute('style', 'opacity: 0.5;');
+    secondBtn.setAttribute('style', 'opacity: 1;');
+    thirdBtn.setAttribute('style', 'opacity: 1;');
     if(sketch.hasChildNodes()){
         reset();
         createSquares(100);
@@ -45,6 +78,9 @@ firstBtn.onclick = () => {
 
 secondBtn.onclick = () => {
     sketch.setAttribute('style', 'grid-template-columns: repeat(16, 1fr);');
+    firstBtn.setAttribute('style', 'opacity: 1;');
+    secondBtn.setAttribute('style', 'opacity: 0.5;');
+    thirdBtn.setAttribute('style', 'opacity: 1;');
     if(sketch.hasChildNodes()){
         reset();
         createSquares(256);
@@ -57,6 +93,9 @@ secondBtn.onclick = () => {
 
 thirdBtn.onclick = () => {
     sketch.setAttribute('style', 'grid-template-columns: repeat(64, 1fr);');
+    firstBtn.setAttribute('style', 'opacity: 1;');
+    secondBtn.setAttribute('style', 'opacity: 1;');
+    thirdBtn.setAttribute('style', 'opacity: 0.5;');
     if(sketch.hasChildNodes()){
         reset();
         createSquares(4096);
